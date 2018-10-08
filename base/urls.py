f412c3b21e5823475aacf334a2f389be313188ee
Harsh Gupta <harshrp94@gmail.com>
2018-10-08 18:05:01 +0100

    Setting up server and data loading to database

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
+
 import ssida_app.views
 
 # Examples:
@@ -14,6 +15,7 @@ urlpatterns = [
     url(r'^$', ssida_app.views.index, name=''),
     url('index', ssida_app.views.index, name='index'),
     url('rawdata', ssida_app.views.rawData, name='rawdata'),
+    url('livedata',ssida_app.views.showRawData,name='showrawdata'),
     url('storeddata', ssida_app.views.storedData, name='storeddata'),
     url(r'^db', ssida_app.views.db, name='db'),
     path('admin/', admin.site.urls),
