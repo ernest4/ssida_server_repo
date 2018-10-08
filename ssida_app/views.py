402d6dbf04dd897f8b1816b74787946f387a34af
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-10-08 13:17:57 +0100

    Displaying raw data

--- ssida_app/views.py
+++ ssida_app/views.py
@@ -8,7 +8,20 @@ def index(request):
     return render(request, 'index.html')
 
 def rawData(request):
-    return render(request, 'rawdata.html')
+    params = request.GET
+
+    #device_id = params['device_id']
+    #latitude = params['latitude']
+    #longitude = params['longitude']
+    #accelerometer_x = params['accelerometer_x']
+    #accelerometer_y = params['accelerometer_y']
+    #accelerometer_z = params['accelerometer_z']
+    #gyroscope_x = params['gyroscope_x']
+    #gyroscope_y = params['gyroscope_y']
+    #gyroscope_z = params['gyroscope_z']
+    #timestamp = params['timestamp']
+
+    return render(request, 'rawdata.html', {'params': params, 'keys': params.keys()})
 
 def storedData(request):
     return render(request, 'storeddata.html')
